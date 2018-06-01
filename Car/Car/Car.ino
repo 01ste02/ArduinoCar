e5e91376d307720e78abebab3a5d8757ffc2959e
Oskar Stenberg <01ste02@gmail.com>
2018-06-01 11:40:32 +0200

    Added sensors, turns right when dist < 10 cm

--- Car/Car/Car.ino
+++ Car/Car/Car.ino
@@ -1,17 +1,21 @@
-#define trigPin 13
-#define echoPin 12
+#define trigPin 12
+#define echoPin 13
+
+long duration, distance;
 
 int pwm_a = 3;  //PWM control for motor outputs 1 and 2 
 int pwm_b = 9;  //PWM control for motor outputs 3 and 4 
 int dir_a = 2;  //direction control for motor outputs 1 and 2 
 int dir_b = 8;  //direction control for motor outputs 3 and 4 
 
-int leftDelay = 250;
-int rightDelay = 250;
+int leftDelay = 150;
+int rightDelay = 150;
 int rotateDelay = 2 * rightDelay;
 
 
 void setup() {
+  //Serial.begin(9600);
+  
   pinMode(pwm_a, OUTPUT);  //Set control pins to be outputs
   pinMode(pwm_b, OUTPUT);
   pinMode(dir_a, OUTPUT);
@@ -25,9 +29,22 @@ void setup() {
 }
 
 void loop() {
-  turnRight();
-  //driveForward(250);
-  delay(1000);
+//  turnRight();
+//  //driveForward(250);
+//  delay(1000);
+  checkSensors();
+  //Serial.println(distance);
+  while (distance > 10) {
+    driveForward(10);
+    checkSensors();
+    //Serial.println(distance);
+  }
+
+  if (distance < 10) {
+    Serial.println("Turn");
+    turnRight();
+    eStop();
+  }
 }
 
 void turnLeft() {
@@ -38,7 +55,7 @@ void turnLeft() {
   analogWrite(pwm_b, 255);
 
   delay(leftDelay);
-  stopCar();
+  stopCarLeft();
 }
 
 void turnRight() {
@@ -49,7 +66,7 @@ void turnRight() {
   analogWrite(pwm_b, 255);
 
   delay(rightDelay);
-  stopCar();
+  stopCarRight();
 }
 
 void driveForward(int duration) {
@@ -86,16 +103,58 @@ void turnAround() {
 }
 
 void stopCar() {
-  digitalWrite(dir_a, LOW); 
-  digitalWrite(dir_b, LOW);  
+  digitalWrite(dir_a, HIGH); 
+  digitalWrite(dir_b, HIGH);  
   
+  analogWrite(pwm_a, 200);  
+  analogWrite(pwm_b, 200);
+
+  delay(15);
   analogWrite(pwm_a, 0);  
   analogWrite(pwm_b, 0);
 }
 
-void checkSensors() {
-  long duration, distance;
+void stopCarLeft() {
+  digitalWrite(dir_a, LOW); 
+  digitalWrite(dir_b, HIGH);  
   
+  analogWrite(pwm_a, 200);  
+  analogWrite(pwm_b, 200);
+
+  delay(15);
+
+  analogWrite(pwm_a, 0);  
+  analogWrite(pwm_b, 0);
+}
+
+void stopCarRight() {
+  digitalWrite(dir_a, HIGH); 
+  digitalWrite(dir_b, LOW);  
+  
+  analogWrite(pwm_a, 200);  
+  analogWrite(pwm_b, 200);
+
+  delay(15);
+
+  analogWrite(pwm_a, 0);  
+  analogWrite(pwm_b, 0);
+}
+
+void eStop() {
+  digitalWrite(dir_a, HIGH); 
+  digitalWrite(dir_b, HIGH);  
+  
+  analogWrite(pwm_a, 200);  
+  analogWrite(pwm_b, 200);
+
+  delay(15);
+
+  analogWrite(pwm_a, 0);  
+  analogWrite(pwm_b, 0);
+  
+}
+
+void checkSensors() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   
@@ -105,6 +164,9 @@ void checkSensors() {
   digitalWrite(trigPin, LOW);
   
   duration = pulseIn(echoPin, HIGH);
+  if (duration > 5820) {
+    duration = 5820;
+  }
   distance = (duration/2) / 29.1;
 }
 
