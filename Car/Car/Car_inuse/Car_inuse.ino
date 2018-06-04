7768f7c4a449cade10d8fed9be1507799ea5bcf9
stenlund <tairstenen@gmail.com>
2018-06-04 14:18:47 +0200

    Left and right sensors added.
    Will turn left if right sensor is detecting.
    Will turn right if left sensor is detecting.

--- Car/Car/Car_inuse/Car_inuse.ino
+++ Car/Car/Car_inuse/Car_inuse.ino
@@ -1,15 +1,21 @@
-#define trigPin 12
-#define echoPin 13
+#define trigPin 12 // fram
+#define trigPin2 11 // höger
+#define trigPin3 7 // vänster
+#define echoPin 13 // fram echopin
+#define echoPin2 10 // vänster echopin
+#define echoPin3 6 // höger echopin
 
-long duration, distance;
+long duration, distance; // fram
+long duration2, distance2; // höger
+long duration3, distance3; // vänster
 
 int pwm_a = 3;  //PWM control for motor outputs 1 and 2 
 int pwm_b = 9;  //PWM control for motor outputs 3 and 4 
 int dir_a = 2;  //direction control for motor outputs 1 and 2 
 int dir_b = 8;  //direction control for motor outputs 3 and 4 
 
-int leftDelay = 150;
-int rightDelay = 150;
+int leftDelay = 30;
+int rightDelay = 30;
 int rotateDelay = 2 * rightDelay;
 
 
@@ -25,6 +31,8 @@ void setup() {
   analogWrite(pwm_b, 100);
 
   pinMode(trigPin, OUTPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(trigPin3, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
@@ -34,16 +42,30 @@ void loop() {
 //  delay(1000);
   checkSensors();
   //Serial.println(distance);
-  while (distance > 10) {
+  while (distance > 30) {
+    delay(50);
     driveForward(10);
     checkSensors();
     //Serial.println(distance);
   }
 
-  if (distance < 10) {
-    Serial.println("Turn");
-    turnRight();
-    eStop();
+  if (distance < 30) { // hinder framför
+    Serial.println("Hinder");
+    if((distance < 30) && (distance3 < 30)) { // om hinder vänster, svänger höger
+      Serial.println("Höger sväng");
+      turnRight();
+      eStop();
+    } else {
+      if((distance < 30) && (distance2 < 30)) { // om hinder höger, svänger vänster
+        Serial.println("Vänster sväng");
+        turnLeft();
+        eStop();
+      } else {
+        Serial.println("Vänder"); // vänder
+        turnAround();
+        eStop();
+      }
+    }
   }
 }
 
@@ -83,8 +105,8 @@ void driveReverse(int duration) {
   digitalWrite(dir_a, HIGH); 
   digitalWrite(dir_b, HIGH);  
   
-  analogWrite(pwm_a, 150);  
-  analogWrite(pwm_b, 150);
+  analogWrite(pwm_a, 70);  
+  analogWrite(pwm_b, 70);
 
   delay(duration);
   stopCar();
@@ -155,18 +177,47 @@ void eStop() {
 }
 
 void checkSensors() {
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW); // fram
   delayMicroseconds(2);
   
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   
   digitalWrite(trigPin, LOW);
+
+  digitalWrite(trigPin2, LOW); // höger
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin2, LOW);
+
+  digitalWrite(trigPin3, LOW); // vänster
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin3, LOW);
   
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH); // fram
   if (duration > 5820) {
     duration = 5820;
   }
   distance = (duration/2) / 29.1;
+
+  duration2 = pulseIn(echoPin2, HIGH); // höger
+  if(duration2 < 5820) {
+    duration2 = 5820;
+  }
+  distance2 = (duration2/2) / 29.1;
+
+  duration3 = pulseIn(echoPin3, HIGH); // vänster
+  if(duration3 < 5820) {
+    duration3 = 5820;
+  }
+  distance3 = (duration3/2) / 29.1;
 }
+
 
