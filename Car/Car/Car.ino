99532718753f14096e60dc7ddd4cc91e14b909af
Oskar Stenberg <01ste02@gmail.com>
2018-06-07 08:14:36 +0200

    Fixed sensors?

--- Car/Car/Car.ino
+++ Car/Car/Car.ino
@@ -1,7 +1,15 @@
 #define trigPin 12
 #define echoPin 13
 
+#define trigPin2 10
+#define echoPin2 11
+
+#define trigPin3 8
+#define echoPin3 7
+
 long duration, distance;
+long duration2, distance2;
+long duration3, distance3;
 
 int pwm_a = 3;  //PWM control for motor outputs 1 and 2
 int pwm_b = 9;  //PWM control for motor outputs 3 and 4
@@ -26,24 +34,39 @@ void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
 }
 
 void loop() {
-  //turnLeft();
-  //driveForward(250);
-  //delay(1000);
   checkSensors();
-  //Serial.println(distance);
-  while (distance > 10) {
-    driveForward(10);
+  while (distance > 20) {
+    driveForward(1);
     checkSensors();
-    //Serial.println(distance);
   }
 
-  if (distance < 10) {
-    Serial.println("Turn");
-    turnRight();
-    //  eStop();
+  if (distance < 21) {
+    eStop();
+    if (distance2 > distance3) {
+      turnRight();
+    } else if (distance2 < distance3) {
+      turnLeft();
+    } else {
+      int randInt = random(0, 2);
+      switch (randInt) {
+        case 0:
+          turnLeft();
+          break;
+        case 1:
+          turnRight();
+          break;
+        case 2:
+          turnAround();
+          break;
+      }
+    }
   }
 }
 
@@ -147,7 +170,7 @@ void eStop() {
   analogWrite(pwm_a, 100);
   analogWrite(pwm_b, 100);
 
-  delay(15);
+  delay(150);
 
   analogWrite(pwm_a, 0);
   analogWrite(pwm_b, 0);
@@ -168,5 +191,39 @@ void checkSensors() {
     duration = 5820;
   }
   distance = (duration / 2) / 29.1;
+
+  //-----------------------------------------------------
+  //Sensor 2
+
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin2, LOW);
+
+  duration2 = pulseIn(echoPin2, HIGH);
+  if (duration2 > 5820) {
+    duration2 = 5820;
+  }
+  distance2 = (duration2 / 2) / 29.1;
+
+  //-----------------------------------------------------
+  //Sensor 3
+
+  digitalWrite(trigPin3, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin3, LOW);
+
+  duration3 = pulseIn(echoPin3, HIGH);
+  if (duration3 > 5820) {
+    duration3 = 5820;
+  }
+  distance3 = (duration3 / 2) / 29.1;
 }
 
