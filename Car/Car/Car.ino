c306bd568f7de1c8a4f7a67eb6b0e2d92e1e7700
Oskar Stenberg <01ste02@gmail.com>
2018-06-13 18:38:29 +0200

    Working 7.4 seconds

--- Car/Car/Car.ino
+++ Car/Car/Car.ino
@@ -4,8 +4,8 @@
 #define MAX_DISTANCE 70 // Maximum distance (in cm) to ping.
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(4, 5, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
-  NewPing(6, 7, MAX_DISTANCE), 
+  NewPing(4, 5, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(6, 7, MAX_DISTANCE),
   NewPing(10, 11, MAX_DISTANCE)
 };
 
@@ -20,7 +20,7 @@ int leftDelay = 415;
 int rightDelay = 415;
 int rotateDelay = 2 * rightDelay;
 
-int leftButton = A0;
+int leftButton = A5;
 int rightButton = A1;
 
 int buttonStateL = 0;
@@ -45,33 +45,37 @@ void setup() {
 void loop() {
   checkSensors();
   Serial.println(distance[0]);
-  
+
   if ((buttonStateL == LOW) && (buttonStateR == LOW)) {
-  if (distance[0] > 20) {
-    driveForward(1);
-    //Serial.println(distance[0]);
-  }
-  checkSensors();
-  
-  if (distance[0] < 20) {
-    eStop();
-    if (distance[1] > distance[2]) {
-      if (distance[0] < 20) {
-        checkSensors();
-        turnLeft(5);
-      }
-    } else if (distance[1] < distance[2]) {
-      if (distance[0] < 20) {
-        checkSensors();
-        turnRight(5);
-      }
-    } else {
-    }
-  }
-  } else if (buttonStateL == HIGH) {
+    if ((distance[1] < 10) && (distance[2] > distance[1]) && (distance[0] > distance[1])) {
+      turnRight(5);
+    } else if ((distance[2] < 10) && (distance[1] > distance[2]) && (distance[0] > distance[2])) {
       turnLeft(5);
+    } else if ((distance[0] > 20) && (distance[1] > 10) && (distance[2] > 10)) {
+      driveForward(1);
+      //Serial.println(distance[0]);
+    }
+    //checkSensors();
+
+    if (distance[0] < 20) {
+      eStop();
+      if (distance[1] > distance[2]) {
+        if (distance[0] < 20) {
+          //checkSensors();
+          turnLeft(5);
+        }
+      } else if (distance[1] < distance[2]) {
+        if (distance[0] < 20) {
+          //checkSensors();
+          turnRight(5);
+        }
+      }
+    }
+
+  } else if (buttonStateL == HIGH) {
+    turnLeft(5);
   } else if (buttonStateR == HIGH) {
-        turnRight(5);
+    turnRight(5);
   }
 }
 
@@ -183,7 +187,7 @@ void eStop() {
 }
 
 void checkSensors() {
-for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
     delay(29); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     Serial.print(i);
     Serial.print("=");
